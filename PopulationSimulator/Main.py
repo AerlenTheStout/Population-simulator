0343ed117e31e3d6526b94e2a96b0e83e48187c7
Theyobros <realtheyobros@gmail.com>
2021-11-16 17:28:41 -0400

    added year counter wsith shitty code

--- PopulationSimulator/Main.py
+++ PopulationSimulator/Main.py
@@ -12,11 +12,15 @@ def changeShelterLabel(*args):
     ST.ShelterLabel.config(text='') # clear label
     ST.ShelterLabel.config(text= "Shelter = " + ST.numOfShelterEntry.get()) # set new label text
 
+def changeYearLabel(*args):
+    ST.yearLabel.config(text='') # clear label
+    ST.yearLabel.config(text="Year " + str(year)) # set new label text
+
 def ST():
 
     #Intalize Tkinter
-    root = tk.Tk()
-    root.title('Ecosystem Population Simulator')
+    ST.root = tk.Tk()
+    ST.root.title('Ecosystem Population Simulator')
 
     #make traceable variables
     ST.WaterVar = tk.StringVar()
@@ -24,14 +28,18 @@ def ST():
     ST.ShelterVar = tk.StringVar()
 
     #make entry boxes
-    ST.numOfWaterEntry = tk.Entry(root, width=10,textvariable=ST.WaterVar)
-    ST.numOfFoodEntry = tk.Entry(root, width=10,textvariable=ST.FoodVar)
-    ST.numOfShelterEntry = tk.Entry(root, width=10,textvariable=ST.ShelterVar)
+    ST.numOfWaterEntry = tk.Entry(ST.root, width=10,textvariable=ST.WaterVar)
+    ST.numOfFoodEntry = tk.Entry(ST.root, width=10,textvariable=ST.FoodVar)
+    ST.numOfShelterEntry = tk.Entry(ST.root, width=10,textvariable=ST.ShelterVar)
 
     #make changeable labels
-    ST.WaterLabel = tk.Label(root,text="Water = 0")
-    ST.FoodLabel = tk.Label(root,text="Food = 0")
-    ST.ShelterLabel = tk.Label(root,text="Shelter = 0")
+    ST.WaterLabel = tk.Label(ST.root,text="Water = 0")
+    ST.FoodLabel = tk.Label(ST.root,text="Food = 0")
+    ST.ShelterLabel = tk.Label(ST.root,text="Shelter = 0")
+
+    #makes year counter label and variable
+    ST.yearLabel = tk.Label(ST.root, text="")
+    global year; year = 0
 
     #change label
     ST.WaterVar.trace('w', changeWaterLabel)
@@ -45,7 +53,15 @@ def ST():
     ST.WaterLabel.grid(row=2,column=1)
     ST.FoodLabel.grid(row=2,column=2)
     ST.ShelterLabel.grid(row=2,column=3)
+    ST.yearLabel.grid(row=1,column=2)
+    ActivateButton = tk.Button(ST.root, padx=10, pady=10,text='Advance', command=activate)
+    
+    ActivateButton.grid(row=4,column=2)
 
-    root.mainloop()
+    ST.root.mainloop()
 
+def activate():
+    global year; year += 1
+    changeYearLabel()
+    
 ST()
